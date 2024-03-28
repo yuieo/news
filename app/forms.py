d46ca7edf4442e1cc23f7273857c4afc95554c10
yv <yusif.gadziev@gmail.com>
2024-03-28 19:03:50 +0500

    remake

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms import StringField, TextAreaField, SelectField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Optional, Email, EqualTo
 
 from .models import Category
 
@@ -11,13 +11,31 @@ def get_categories():
 
 
 class NewsForm(FlaskForm):
-    title = StringField(
-        'Название',
-        validators=[DataRequired(message="Поле не должно быть пустым"),
-                    Length(max=255, message='Введите заголовок длиной до 255 символов')]
-    )
-    text = TextAreaField(
-        'Текст',
-        validators=[DataRequired(message="Поле не должно быть пустым")])
+    title = StringField('Название', validators=[DataRequired(message="Поле не должно быть пустым"),
+                                                Length(max=255, message='Введите заголовок длиной до 255 символов')])
+    text = TextAreaField('Текст', validators=[DataRequired(message="Поле не должно быть пустым")])
     category = SelectField('Категория', choices=get_categories, validators=[Optional()])
     submit = SubmitField('Добавить')
+
+
+class CategoryForm(FlaskForm):
+    title = StringField('Название', validators=[DataRequired(message="Поле не должно быть пустым"),
+                                                Length(max=255, message='Введите заголовок длиной до 255 символов')])
+    submit = SubmitField('Добавить')
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Имя пользователя", validators=[DataRequired()])
+    password = PasswordField("Пароль", validators=[DataRequired()])
+    remember = BooleanField("Запомнить меня")
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Имя пользователя", validators=[DataRequired()])
+    name = StringField("Имя", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email(message='Некорректный email')])
+    password = PasswordField("Пароль", validators=[DataRequired()])
+    password2 = PasswordField("Повторите пароль",
+                              validators=[DataRequired(), EqualTo('password', message='Пароли не совпадают')])
+    submit = SubmitField('Зарегистрироваться')
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
 
 from . import app, db
-from .forms import NewsForm
-from .models import Category, News
+from .forms import NewsForm, LoginForm, RegistrationForm, CategoryForm
+from .models import Category, News, User
 
 
 @app.route('/')
@@ -22,7 +23,6 @@ def news_detail(id):
                            news=news,
                            categories=categories)
 
-# продолжение дальше...
 
 @app.route('/category/<int:id>')
 def news_in_category(id):
@@ -37,6 +37,7 @@ def news_in_category(id):
 
 
 @app.route('/add_news', methods=['GET', 'POST'])
+@login_required
 def add_news():
     form = NewsForm()
     categories = Category.query.all()
@@ -51,3 +52,62 @@ def add_news():
     return render_template('add_news.html',
                            form=form,
                            categories=categories)
+
+
+@app.route('/add_category', methods=['GET', 'POST'])
+@login_required
+def add_category():
+    form = CategoryForm()
+    categories = Category.query.all()
+    if form.validate_on_submit():
+        category = Category()
+        category.title = form.title.data
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_category.html',
+                           form=form,
+                           categories=categories)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    categories = Category.query.all()
+    if form.validate_on_submit():
+        user = User.query.filter(User.username == form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('Вход выполнен!', 'alert-success')
+            return redirect(url_for('index'))
+        else:
+            flash('Вход не выполнен!', 'alert-danger')
+    return render_template('login.html', form=form, categories=categories)
+
+
+@app.route('/registration/', methods=['GET', 'POST'])
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    categories = Category.query.all()
+    if form.validate_on_submit():
+        user = User()
+        user.username = form.username.data
+        user.name = form.name.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Регистрация прошла успешно!', 'alert-success')
+        return redirect(url_for('login'))
+    return render_template('registration.html', form=form, categories=categories)
+
+
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
